40aaef96b87bee0737ef8eddf259fa761f763532
Juan Padilla <padilla@speedycontrol.com>
2020-12-09 19:33:47 +0100

    Alta inicial

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Alguna cosa ha ido mal leyendo el fichero");
+
+    println!("With text:\n{}", contents);
+
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query, filename }
+    }
+}
+
