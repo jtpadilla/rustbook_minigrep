9d278825d636f00519259eb20f0e7baa7064cc99
Juan Padilla <padilla@speedycontrol.com>
2020-12-10 23:43:28 +0100

    Mejoras

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,23 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Alguna cosa ha ido mal leyendo el fichero");
-
-    println!("With text:\n{}", contents);
-
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Config { query, filename }
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
+
 }
 
